b9412b9856d2fac5b6a6cc5de4dcb160810244cd
unconv <unconventionalcoding@gmail.com>
2023-07-19 06:28:56 +0300

    changed default model

--- ChatGPT.php
+++ ChatGPT.php
@@ -98,7 +98,7 @@ class ChatGPT {
 
     public function response( bool $raw_function_response = false ) {
         $fields = [
-            "model" => "gpt-4-0613",
+            "model" => "gpt-3.5-turbo-16k-0613",
             "messages" => $this->messages,
         ];
 
